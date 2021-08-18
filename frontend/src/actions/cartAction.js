3ba64dce05673414be4fce0b604f71d513474d2b
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2021-08-18 23:51:57 +0530

    feat - backend url changed

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/products/${id}`);
+  const { data } = await axios.get(`https://zitsby-e-commerce-backend.shaileshweb.repl.co/products/${id}`);
   console.log(data, "shailesh");
 
   dispatch({
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,7 +4,7 @@ export const productListAction = () => async (dispatch) => {
   try {
     dispatch({ type: "PRODUCT_LIST_REQUEST" });
 
-    const { data } = await axios.get("http://localhost:5000/products");
+    const { data } = await axios.get("https://zitsby-e-commerce-backend.shaileshweb.repl.co/products");
 
     dispatch({ type: "PRODUCT_LIST_SUCCESS", payload: data });
   } catch (error) {
@@ -15,7 +15,7 @@ export const productDetailsAction = (id) => async (dispatch) => {
   try {
     dispatch({ type: "PRODUCT_DETAILS_REQUEST" });
 
-    const { data } = await axios.get(`http://localhost:5000/products/${id}`);
+    const { data } = await axios.get(`https://zitsby-e-commerce-backend.shaileshweb.repl.co/products/${id}`);
 
     dispatch({ type: "PRODUCT_DETAILS_SUCCESS", payload: data });
   } catch (error) {
